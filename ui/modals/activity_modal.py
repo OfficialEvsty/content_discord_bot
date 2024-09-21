1ae42454571418e7c5e7ddb7a2d3e853049b2fa6
Evsty <75947237+OfficialEvsty@users.noreply.github.com>
2024-09-21 23:28:34 +0300

    INPUT-DATE-FORMAT-CHANGED

--- ui/modals/activity_modal.py
+++ ui/modals/activity_modal.py
@@ -15,8 +15,8 @@ from validation.date_validation import check_date_range
 class DateInputModal(BaseModal):
     def __init__(self):
         super().__init__("Укажите диапазон дат")
-        self.add_item(TextInput(label="Начальная дата (YYYY-MM-DD)", placeholder="2024-05-24", style=TextStyle.short))
-        self.add_item(TextInput(label="Конечная дата (YYYY-MM-DD)", placeholder="2024-05-25", style=TextStyle.short))
+        self.add_item(TextInput(label="Начальная дата (DD-MM-YYYY)", placeholder="24-05-2024", style=TextStyle.short))
+        self.add_item(TextInput(label="Конечная дата (DD-MM-YYYY)", placeholder="25-05-2024", style=TextStyle.short))
 
         self.start = None
         self.end = None
@@ -26,7 +26,7 @@ class DateInputModal(BaseModal):
         start_date = self.children[0].value
         end_date = self.children[1].value
 
-        date_format: str = "%Y-%m-%d"
+        date_format: str = "%d-%m-%Y"
         if not check_date_range(start_date, end_date, date_format):
             await interaction.response.send_message("Введите время в правильном формате и в правильном диапазоне start_date < end_date")
             return
