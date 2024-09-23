b054ae03fec584f37bca3d6d5e7d7a865a874335
Evsty <75947237+OfficialEvsty@users.noreply.github.com>
2024-09-23 22:48:13 +0300

    recognizing nicknames fix

--- utilities/screenshot_handling.py
+++ utilities/screenshot_handling.py
@@ -83,10 +83,8 @@ async def recognize_nicknames_on_image(lang_list, image_to_recognize, nicknames)
         if len(occurrences) == 0 and similar_letters is not None:
             rus, eng = translate_substring_to_similar_lang(similar_letters, substring)
             for substr in [rus, eng]:
-                if substr:
-                    print(f"{substr} skip")
+                if not substr:
                     continue
-                print(f"{substr} proccess")
                 translate_pattern = f"^{re.escape(substr)}"
                 occurrences = find_nicknames_by_predicate(translate_pattern, visited_members, nicknames)
 
