f1715112effc1b30d14ab8a285678b2ec56285bd
Evsty <75947237+OfficialEvsty@users.noreply.github.com>
2024-10-03 23:43:03 +0300

    rework panel structure

--- ui/views/manage_panel_view.py
+++ ui/views/manage_panel_view.py
@@ -12,11 +12,11 @@ from ui.views.base_view import BaseView, CancelledView
 
 
 class ManagerPanelView(CancelledView):
-    def __init__(self, controller, message = None):
+    def __init__(self, controller, message = None, dates = None):
         super().__init__()
         self.message = message
         self.controller = controller
-        self.dates = (None, None)
+        self.dates = dates
         self.nickname_activities_percent = None
         self.activity_button = Button(label="Активка")
         self.salary_button = Button(label="Зарплата")
@@ -184,7 +184,7 @@ class PaginatorView(BaseView):
         await self.update(interaction)
 
     async def back_callback(self, interaction: discord.Interaction):
-        view = ManagerPanelView(self.controller, self.message)
+        view = ManagerPanelView(self.controller, self.message, self.dates)
         embed =ManagerEmbed(interaction.user, ">>> *Панель - лучший инструмент для просмотра статистики активностей и зарплат мемберов*", is_formatted=True)
         await interaction.response.edit_message(view=view, embed=embed, attachments=[embed.file])
         self.stop()
