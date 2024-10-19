cfb4e9d30e0d50ef4d874049b069c9df44063e04
Evsty <75947237+OfficialEvsty@users.noreply.github.com>
2024-10-19 21:58:13 +0300

    guilds update

--- web/nickname_parsing.py
+++ web/nickname_parsing.py
@@ -25,9 +25,14 @@ async def get_nicknames_from_archeage_website(archeage_config):
         response.encoding = 'utf-8'
         json_file = json.loads(response.content)
         all_union_users = json_file['data']['candidates'][archeage_config['server_id']][archeage_config['union_id']]
+        print(all_union_users)
+        print("\n\n\n")
+
         archeage_unicode_escape_encoding_guilds = [to_unicode_escape(guild_name) for guild_name in archeage_config['guilds']]
+        print(archeage_unicode_escape_encoding_guilds)
         for user in all_union_users:
             if user['guild'] in archeage_unicode_escape_encoding_guilds:
+                print(f"user guild: {user['guild']}, guilds: {archeage_unicode_escape_encoding_guilds}")
                 tag.add_member(user['name'], user['guild'])
     else:
         logger.warning(f"Error with status code = {response.status_code}")
